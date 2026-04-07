347c5a5d356f0f16092cae75dd249d8b059252eb
platonandrei-dot <platon.andrei2010@gmail.com>
2026-04-07 22:46:38 +0300

    am urmair un video de la broganmpratt cu tuch senzori

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuchSensorPractice.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+
+
+@TeleOp
+public class TuchSensorPractice extends OpMode {
+
+    TestBench bench = new TestBench();
+
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+
+    }
+
+
+    @Override
+    public void loop() {
+      if (bench.isTouchSensorPressed()){
+          telemetry.addData("Touch Sensor","Pressed!");
+      }
+      else telemetry.addData("Touch Sensor", "NOT Pressed!");
+
+    }
+}
+
